e6d611c2ceb5d4d73af336031bc6cfea967b415a
mgranciano <granciano_2010@hotmail.com>
2023-10-08 22:39:58 -0600

    fix:resolve conflict

--- back-end/src/modules/stock/stock.resolver.ts
+++ back-end/src/modules/stock/stock.resolver.ts
@@ -1,7 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, ArgsType, Context, Field, InputType, Int, Mutation, ObjectType, Resolver } from '@nestjs/graphql';
 import { Query } from '@nestjs/graphql';
 import { Stock } from '../../models';
 import { StockService } from './stock.service';
+
+
+//@InputType("PaginationArgs")
+//@ObjectType("PaginationArgs")
+@ArgsType()
+export class PaginationArgs {
+  @Field(() => Int , { nullable: true })
+  offset?: number;
+
+  @Field(() => Int ,{ nullable: true })
+  limit?: number;
+}
+
+//Stock( @Args() pagination: PaginationArgs ){
 
 
 @Resolver()
@@ -9,8 +23,16 @@ export class StockResolver {
 
     constructor(private stockService: StockService) { }
 
-    @Query((returns) => [Stock])
-    Stock() {
+    @Query(() => [Stock])
+    Stock(){
         return this.stockService.findAll();
+    }
+
+    @Mutation(() => [Stock], { name: 'getAll' })
+    createStock(@Args({ name: 'limit', type: () => Int, nullable: true }) limit: number,
+                @Args({ name: 'offset', type: () => Int, nullable: true }) offset: number){
+        console.log(limit)
+        console.log(offset)
+        return this.stockService.find(limit,offset);
     }
 }
--- back-end/src/modules/stock/stock.service.ts
+++ back-end/src/modules/stock/stock.service.ts
@@ -13,4 +13,16 @@ export class StockService {
     findAll(): Promise<Stock[]> {
         return this.postsRepository.find();
     }
+    async find(limit?: number, offset: number =0 ): Promise <Stock[]> {
+
+        const[data, total] = await this.postsRepository.findAndCount({
+            order: {
+                id: "ASC",
+            },
+            take: limit,
+            skip: offset === 0? offset : limit * offset
+        })
+
+        return data;
+    }
 }
